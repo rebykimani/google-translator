12172f4c5db1cc94a5d995030114a0ecab0630c7
beckireby15@gmail.com <beckireby15@gmail.com>
2022-09-06 11:31:05 +0300

    added DOM render functions

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ function handleClick(e) {
     language: e.target.language.user - Input.value,
   };
 }
+renderTranslateLanguage(languageObj);
+
+//DOM render Functions
+function renderTranslateLanguage(language) {
+  //build translated language list
+  let card = document.createElement("li");
+  card.className = "card";
+  card.innerHTML = (
+    <div class="content">
+      <h4>${translate.language}</h4>
+      <p></p>
+    </div>
+  );
+}
