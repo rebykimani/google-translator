dbcb5f77ddb6520fdb1e3aff2948c77a9b36a2fc
beckireby15@gmail.com <beckireby15@gmail.com>
2022-09-06 14:48:51 +0300

    added fetch request

--- src/index.js
+++ src/index.js
@@ -29,3 +29,77 @@ function renderTranslateLanguage(language) {
 }
 
 //fetch request
+const { default: axios } = require("axios");
+const axios = require(axios);
+
+let result = document.getElementById("display-all");
+let selectLanguage = document.getElementById("select-language");
+let searchBtn = document.getElementById("search");
+
+let url = "'https://google-translate1.p.rapidapi.com/language/translate/v2'";
+
+const translateFunction = async () => {
+  const data = {};
+  await axios
+    .post(`${url}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((e) => console.log("Error: ", e));
+};
+
+let languages = {
+  Swahili: "sw",
+  Afrikaans: "af",
+  Albanian: "sq",
+  Amharic: "am",
+  Arabic: "ar",
+  Armenian: "hy",
+  Azerbaijani: "az",
+  Basque: "eu",
+  Belarusian: "be",
+  Bengali: "bn",
+  Bosnian: "bs",
+  Bulgarian: "bg",
+  Catalan: "ca",
+  Cebuano: "ceb (ISO-639-2)",
+  Chinese: "zh-CN (BCP-47)",
+  Chinese: "zh-TW (BCP-47)",
+  Corsican: "co",
+  Croatian: "hr",
+  Czech: "cs",
+  Danish: "da",
+  Dutch: "nl",
+  English: "en",
+  Esperanto: "eo",
+  Estonian: "et",
+  Finnish: "fi",
+  French: "fr",
+  Frisian: "fy",
+  Galician: "gl",
+  Georgian: "ka",
+  German: "de",
+  Greek: "el",
+  Gujarati: "gu",
+  Hausa: "ha",
+  Hawaiian: "haw",
+  Hebrew: "he",
+  Hindi: "hi",
+  Persian: "fa",
+  Polish: "pl",
+  Punjabi: "pa",
+  Romanian: "ro",
+  Russian: "ru",
+  Samoan: "sm",
+  Serbian: "sr",
+  Sesotho: "st",
+  Shona: "sn",
+  Sindhi: "sd",
+  Slovak: "sk",
+  Slovenian: "sl",
+  Somali: "so",
+  Spanish: "es",
+  Sundanese: "su",
+  Yoruba: "yo",
+  Zulu: "zu",
+};
