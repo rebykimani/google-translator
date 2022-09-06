257f5435c565384aafc8f437db48f4239506c15f
beckireby15@gmail.com <beckireby15@gmail.com>
2022-09-06 17:27:09 +0300

    display language list on select

--- src/index.js
+++ src/index.js
@@ -1,105 +1,149 @@
 //add event listeners
-document
-  .querySelector(".search-container")
-  .addEventListener("click", handleClick);
 
+const languages = {
+  sw: "Swahili",
+  af: "Afrikaans",
+  sq: " Albanian",
+  am: "Amharic",
+  ar: "Arabic",
+  hy: "Armenian",
+  az: "Azerbaijani",
+  eu: "Basque",
+  be: "Belarusian",
+  bn: "Bengali",
+  bs: "Bosnian",
+  bg: "Bulgarian",
+  ca: "Catalan",
+  co: "Corsican",
+  hr: "Croatian",
+  cs: "Czech",
+  da: "Danish",
+  nl: "Dutch",
+  en: "English",
+  eo: "Esperanto",
+  et: "Estonian",
+  fi: "Finnish",
+  fr: "French:",
+  fy: "Frisian",
+  g: "Galician",
+  de: "German",
+  el: "Greek",
+  gu: "Gujarati",
+  ha: "Hausa",
+  haw: "Hawaiian",
+  he: "Hebrew",
+  hi: "Hindi",
+  fa: "Persian",
+  pl: "Polish",
+  pa: "Punjabi",
+  ro: "Romanian",
+  ru: "Russian",
+  sm: "Samoan",
+  sr: "Serbian",
+  st: "Sesotho",
+  sn: "Shona",
+  sd: "Sindhi",
+  sk: "Slovak",
+  sl: "Slovenian",
+  so: "Somali",
+  es: "Spanish",
+  su: "Sundanese",
+  yo: "Yoruba",
+  zu: "Zulu",
+};
+const input = document.querySelector("input");
+const log = document.getElementById("typed-text");
+const initialLang = document.getElementById("initial-lang");
+const translatedData = document.getElementById("translated-text");
+let textToDetect = "";
+let initialLanguage = "";
+
+input.addEventListener("change", handleOnChange);
 //event handlers
-function handleClick(e) {
-  e.preventDefault();
-  let languageObj = {
-    language: e.target.language.user - Input.value,
-  };
+function handleOnChange(e) {
+  //   e.preventDefault();
+  log.textContent = e.target.value;
+  console.log("TEXT INPUT: ", e.target.value);
+  textToDetect = e.target.value;
+  detectFunction(textToDetect);
 }
-renderTranslateLanguage(languageObj);
-
-//DOM render Functions
-function renderTranslateLanguage(language) {
-  //build translated language list
-  let card = document.createElement("li");
-  card.className = "card";
-  card.innerHTML = (
-    <div class="content">
-      <h4>${translate.language}</h4>
-      <p></p>
-    </div>
+// this adds a list of languages to a select component
+const select = document.getElementById("select-language");
+for (language in languages) {
+  select.options[select.options.length] = new Option(
+    languages[language],
+    language
   );
-  //add language card to list
-  document.querySelector("#language-typed").appendChild(card);
-  document.querySelector("#display-all").appendChild(card);
+}
+
+//This checks the selected language and submits for translation
+
+const selectedLang = document.getElementById("select-language");
+selectedLang.addEventListener("change", handleLanguageTranslate);
+function handleLanguageTranslate(e) {
+  console.log("SELECTED LANG: ", e.target.value);
+  translateFunction(e.target.value);
 }
 
 //fetch request
-const { default: axios } = require("axios");
-const axios = require(axios);
 
 let result = document.getElementById("display-all");
-let selectLanguage = document.getElementById("select-language");
-let searchBtn = document.getElementById("search");
-
-let url = "'https://google-translate1.p.rapidapi.com/language/translate/v2'";
-
-const translateFunction = async () => {
-  const data = {};
-  await axios
-    .post(`${url}`)
-    .then((response) => {
-      return response.data;
-    })
-    .catch((e) => console.log("Error: ", e));
+const headers = {
+  "content-type": "application/x-www-form-urlencoded",
+  "Accept-Encoding": "application/gzip",
+  "X-RapidAPI-Key": "a864d1ff18msh4ad633aeb1d688bp1d0d99jsnbce0d56743d1",
+  "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
 };
 
-let languages = {
-  Swahili: "sw",
-  Afrikaans: "af",
-  Albanian: "sq",
-  Amharic: "am",
-  Arabic: "ar",
-  Armenian: "hy",
-  Azerbaijani: "az",
-  Basque: "eu",
-  Belarusian: "be",
-  Bengali: "bn",
-  Bosnian: "bs",
-  Bulgarian: "bg",
-  Catalan: "ca",
-  Cebuano: "ceb (ISO-639-2)",
-  Chinese: "zh-CN (BCP-47)",
-  Chinese: "zh-TW (BCP-47)",
-  Corsican: "co",
-  Croatian: "hr",
-  Czech: "cs",
-  Danish: "da",
-  Dutch: "nl",
-  English: "en",
-  Esperanto: "eo",
-  Estonian: "et",
-  Finnish: "fi",
-  French: "fr",
-  Frisian: "fy",
-  Galician: "gl",
-  Georgian: "ka",
-  German: "de",
-  Greek: "el",
-  Gujarati: "gu",
-  Hausa: "ha",
-  Hawaiian: "haw",
-  Hebrew: "he",
-  Hindi: "hi",
-  Persian: "fa",
-  Polish: "pl",
-  Punjabi: "pa",
-  Romanian: "ro",
-  Russian: "ru",
-  Samoan: "sm",
-  Serbian: "sr",
-  Sesotho: "st",
-  Shona: "sn",
-  Sindhi: "sd",
-  Slovak: "sk",
-  Slovenian: "sl",
-  Somali: "so",
-  Spanish: "es",
-  Sundanese: "su",
-  Yoruba: "yo",
-  Zulu: "zu",
+const detectFunction = async (text) => {
+  const data = {};
+  console.log("DATA TO DETECT: ", text);
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://google-translate1.p.rapidapi.com/language/translate/v2/detect",
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "Accept-Encoding": "application/gzip",
+      "X-RapidAPI-Key": "a864d1ff18msh4ad633aeb1d688bp1d0d99jsnbce0d56743d1",
+      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    },
+    data: {
+      q: `${text}`,
+    },
+  };
+  $.ajax(settings).done(function (response) {
+    console.log("RESPONSE FROM API CALL: ", response.data.detections);
+    const resp = response.data.detections[0][0].language;
+    initialLang.textContent = languages[resp];
+    initialLanguage = resp;
+  });
+};
+
+const translateFunction = async (text) => {
+  const data = {};
+  console.log("LANG TO TRANSLATE: ", text);
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "Accept-Encoding": "application/gzip",
+      "X-RapidAPI-Key": "a864d1ff18msh4ad633aeb1d688bp1d0d99jsnbce0d56743d1",
+      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    },
+    data: {
+      q: `${textToDetect}`,
+      source: initialLanguage,
+      target: text,
+    },
+  };
+  $.ajax(settings).done(function (response) {
+    console.log("RESPONSE FROM API CALL: ", response.data);
+    const resp = response.data.translations[0].translatedText;
+    translatedData.textContent = resp;
+  });
 };
